b7b0546af5aed4c60f09ab88c12d6ee6da7141e8
James Austin <jamesaustin@mac.com>
2017-05-23 21:50:46 +0100

    Dsiable processing.

--- nebula.sh
+++ nebula.sh
@@ -2,6 +2,7 @@
 time python3 nebula.py nebula.json \
 	--verbose \
 	--path assets/final \
-	--progress assets/progress/pngs \
-	--noise assets/octave/octave.png 
-open .
+	--noise assets/octave/octave.png \
+	&& open .
+
+#--progress assets/progress/pngs \
