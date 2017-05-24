1fbfb47d2330a87059d9cb040cf5e692999ae271
James Austin <jamesaustin@mac.com>
2017-05-24 18:47:22 +0100

    Tidy running script.

--- nebula.sh
+++ nebula.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 make all
 time python3 nebula.py nebula.json assets/progress
-time ./simulate assets/progress/000.nebula.txt assets/octave/octave.png assets/final/000.nebula.sim.png
+time ./simulate assets/progress/000.nebula.txt assets/octave/octave.png assets/final/000.nebula.png
