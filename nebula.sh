bf84b78bcfdc04187af8ddd0976567373a308097
James Austin <jamesaustin@mac.com>
2017-05-26 09:46:09 +0100

    More verbose script.

--- nebula.sh
+++ nebula.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 make all
-time python3 nebula.py nebula.json assets/progress
+time python3 nebula.py --verbose nebula.json assets/progress
+ls -la assets/progress
 time ./simulate assets/progress/000.nebula.txt assets/octave/octave.png assets/final/000.nebula.png
