6f36f1f8897a6ba6873ab727c8a67e12e161091d
James Austin <jamesaustin@mac.com>
2017-06-14 21:57:38 +0100

    Don't generate listing. Too verbose.

--- nebula.sh
+++ nebula.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 make all
 time python3 nebula.py --verbose nebula.json assets/progress
-ls -la assets/progress
 time build/bin/simulate_c assets/progress/000.nebula.txt assets/octave/octave.png assets/final/000.nebula.c.png
 time build/bin/simulate assets/progress/000.nebula.txt assets/octave/octave.png assets/final/000.nebula.cpp.png
